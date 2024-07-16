14988206ad4b549002f73578ebd451a7f86a3f9f
HarshPatiL11 <patilharsh497@gmail.com>
2024-07-16 14:40:24 +0530

    backend start

---
+++ ex.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const ex = () => {
+  return (
+    <div>
+        <h1>heading</h1>
+        <div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+  )
+}
